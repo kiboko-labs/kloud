86d4d7f53b7cdbaecf4abb9dbab46f4091de7f2d
Grégory Planchat <gregory@kiboko.fr>
2020-03-23 11:08:52 +0100

    Ajout du support oroPlatform ^1.8 pour DiamanteDesk

--- builder/src/Domain/Stack/OroPlatform/FilesAwareTrait.php
+++ builder/src/Domain/Stack/OroPlatform/FilesAwareTrait.php
@@ -4,7 +4,8 @@ namespace Builder\Domain\Stack\OroPlatform;
 
 use Builder\Domain\Stack\DTO;
 use Builder\Domain\Stack\Resource;
-use PhpCsFixer\Finder;
+use Composer\Semver\Semver;
+use Symfony\Component\Finder\Finder;
 
 trait FilesAwareTrait
 {
@@ -18,14 +19,60 @@ trait FilesAwareTrait
      */
     private function findFilesToCopy(DTO\Context $context, string $path): DTO\FileCommandInterface
     {
-        $stackPath = sprintf(
-            '%s/%s/%s/%s/%s',
-            $this->stacksPath,
-            'orocommerce', //$context->application,
-            'ee', //$context->isEnterpriseEdition ? 'ee' : 'ce',
-            $context->applicationVersion,
-            pathinfo($path, PATHINFO_DIRNAME)
-        );
+        if (in_array($context->application, ['oroplatform', 'orocrm'], true)
+            && Semver::satisfies($context->applicationVersion, '>=1.8 <2.0')
+        ) {
+            $stackPath = sprintf(
+                '%s/%s/%s/%s/%s',
+                $this->stacksPath,
+                'oroplatform', // $context->application,
+                'ce', // $context->isEnterpriseEdition ? 'ee' : 'ce',
+                '1.8', // $context->applicationVersion,
+                pathinfo($path, PATHINFO_DIRNAME)
+            );
+        } else if (in_array($context->application, ['oroplatform', 'orocrm'], true)
+            && Semver::satisfies($context->applicationVersion, '>=2.0 <3.0')
+        ) {
+            $stackPath = sprintf(
+                '%s/%s/%s/%s/%s',
+                $this->stacksPath,
+                'oroplatform', // $context->application,
+                'ce', // $context->isEnterpriseEdition ? 'ee' : 'ce',
+                '2.6', // $context->applicationVersion,
+                pathinfo($path, PATHINFO_DIRNAME)
+            );
+        } else if (in_array($context->application, ['marello'], true)
+            && Semver::satisfies($context->applicationVersion, '>=1.0 <2.0')
+        ) {
+            $stackPath = sprintf(
+                '%s/%s/%s/%s/%s',
+                $this->stacksPath,
+                'oroplatform', // $context->application,
+                'ce', // $context->isEnterpriseEdition ? 'ee' : 'ce',
+                '1.8', // $context->applicationVersion,
+                pathinfo($path, PATHINFO_DIRNAME)
+            );
+        } else if (in_array($context->application, ['marello'], true)
+            && Semver::satisfies($context->applicationVersion, '>=2.0 <3.0')
+        ) {
+            $stackPath = sprintf(
+                '%s/%s/%s/%s/%s',
+                $this->stacksPath,
+                'oroplatform', // $context->application,
+                'ce', // $context->isEnterpriseEdition ? 'ee' : 'ce',
+                '2.6', // $context->applicationVersion,
+                pathinfo($path, PATHINFO_DIRNAME)
+            );
+        } else {
+            $stackPath = sprintf(
+                '%s/%s/%s/%s/%s',
+                $this->stacksPath,
+                'orocommerce', //$context->application,
+                'ee', //$context->isEnterpriseEdition ? 'ee' : 'ce',
+                $context->applicationVersion,
+                pathinfo($path, PATHINFO_DIRNAME)
+            );
+        }
         $finder = (new Finder())
             ->in($stackPath)
             ->ignoreDotFiles(false)
