64dca58ec51117a9c7ac87963dde4ee3ade24f93
Grégory Planchat <gregory@kiboko.fr>
2020-11-15 16:22:37 +0100

    Refactored a bit the test fixtures architecture

--- test/Stack/Assertion/AssertTrait.php
+++ test/Stack/Assertion/AssertTrait.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace test\Kiboko\Cloud\Assertion;
+namespace test\Kiboko\Cloud\Stack\Assertion;
 
 use PHPUnit\Framework\Constraint\Constraint;
 use PHPUnit\Framework\Constraint\LogicalNot;
