71e2bee4abbdb8ebc8b1776ae4902b2f4530e136
Grégory Planchat <gregory@kiboko.fr>
2020-11-19 13:06:51 +0100

    Changed the minimal PHP versions for Oro 4.2

--- src/Domain/Packaging/Platform/Edition/OroCRMCommunityEditionRepository.php
+++ src/Domain/Packaging/Platform/Edition/OroCRMCommunityEditionRepository.php
@@ -88,8 +88,8 @@ final class OroCRMCommunityEditionRepository implements EditionRepositoryInterfa
                 'oroplatform',
                 '4.2',
                 'ce',
-                '>=7.3 <8.0',
-                '>=7.3 <=8.0',
+                '>=7.4 <8.0',
+                '>=7.4 <=8.0',
             )
         );
     }
--- src/Domain/Packaging/Platform/Edition/OroCommerceEnterpriseEditionRepository.php
+++ src/Domain/Packaging/Platform/Edition/OroCommerceEnterpriseEditionRepository.php
@@ -52,8 +52,8 @@ final class OroCommerceEnterpriseEditionRepository implements EditionRepositoryI
                 'orocommerce',
                 '4.2',
                 'ce',
-                '>=7.3 <8.0',
-                '>=7.3 <=8.0',
+                '>=7.4 <8.0',
+                '>=7.4 <=8.0',
             )
         );
     }
